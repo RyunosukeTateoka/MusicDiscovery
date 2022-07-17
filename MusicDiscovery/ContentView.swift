0de1d71982525a3d49a2c3e682bb9240facf77d7
Ryunosuke Tateoka <shabadabastyle@gmail.com>
2022-07-17 20:04:19 +0900

    Initial Commit

---
+++ MusicDiscovery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicDiscovery
+//
+//  Created by 舘岡龍之介 on 2022/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicDiscovery/MusicDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicDiscoveryApp.swift
+//  MusicDiscovery
+//
+//  Created by 舘岡龍之介 on 2022/07/17.
+//
+
+import SwiftUI
+
+@main
+struct MusicDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
